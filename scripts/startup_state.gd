75d53745eea590331f5e2e221cf57d1d74e8b673
Jessica Skinner <153477627+BitDeserty@users.noreply.github.com>
2025-04-22 23:06:34 -0700

    Add new title splash and some fading code to fade it out

--- scripts/startup_state.gd
+++ scripts/startup_state.gd
@@ -2,7 +2,8 @@ extends State
 class_name StartupState
 
 @onready var bank_instance = $"../../Bank" as Bank
-
+var tween : Tween
+@export var fade_duration : float = 1.0    # Duration for the fade out
 
 func Enter():
 	print("Entering Startup State")
@@ -40,6 +41,15 @@ func Exit():
 	$"../../ReelSet".disconnect("_reels_homed", Callable(self, "_on_reels_homed"))
 
 func _on_reels_homed():
-	$TitleSplash.visible = false
+	#$TitleSplash.visible = false
+	fade_out()
 	$"../IdleState/InsertBillTip".visible = true
 	$"..".change_state(self, "IdleState")
+	
+# Function to fade out the texture
+func fade_out() -> void:
+	# Ensure the texture is visible and has a material to modify
+	if $TitleSplash:
+		# Start the fade animation using the tween
+		var tween = get_tree().create_tween()
+		tween.tween_property($TitleSplash, "modulate:a", 0.0, fade_duration)
